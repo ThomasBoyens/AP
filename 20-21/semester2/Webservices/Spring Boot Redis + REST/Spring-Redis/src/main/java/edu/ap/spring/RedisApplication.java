1e903e2d2680f336c118b02e283679aa73341ab0
thomas boyens <thomas.boyens@hotmail.com>
2021-03-24 15:32:13 +0100

    update

--- 20-21/semester2/Webservices/Spring Boot Redis + REST/Spring-Redis/src/main/java/edu/ap/spring/RedisApplication.java
+++ 20-21/semester2/Webservices/Spring Boot Redis + REST/Spring-Redis/src/main/java/edu/ap/spring/RedisApplication.java
@@ -18,14 +18,14 @@ import edu.ap.spring.redis.RedisService;
 
 @SpringBootApplication
 public class RedisApplication {
-	
+
 	private String CHANNEL = "edu:ap:redis";
 	@Autowired
 	private RedisService service;
-	
+
 	@Bean
 	RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
-											MessageListenerAdapter listenerAdapter) {
+			MessageListenerAdapter listenerAdapter) {
 
 		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
 		container.setConnectionFactory(connectionFactory);
@@ -38,49 +38,49 @@ public class RedisApplication {
 	MessageListenerAdapter listenerAdapter(RedisController controller) {
 		return new MessageListenerAdapter(controller, "onMessage");
 	}
-	
+
 	@Bean
-  	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return (args) -> {
 
 			// empty db
 			this.service.flushDb();
 
 			// messaging
-	 		service.sendMessage(CHANNEL, "Hello from Spring Boot");
-	 		
-	 		// keys
-	 		service.setKey("edu:ap:test", "Key from Spring Boot");
-	 		System.out.println(service.getKey("edu:ap:test"));
-	 		
-	 		// bitops
-	 		String bitKey = "edu:ap:bits";
-	 		service.setBit(bitKey, 73, true);
-	 		service.setBit(bitKey, 85, true);
-	 		service.setBit(bitKey, 90, true);
-	 		System.out.println(bitKey + ", bit 73 : " + service.getBit(bitKey, 73));
-	 		System.out.println(bitKey + ", bit count : " + service.bitCount(bitKey));
-	 		
+			service.sendMessage(CHANNEL, "Hello from Spring Boot");
+
+			// keys
+			service.setKey("edu:ap:test", "Key from Spring Boot");
+			System.out.println(service.getKey("edu:ap:test"));
+
+			// bitops
+			String bitKey = "edu:ap:bits";
+			service.setBit(bitKey, 73, true);
+			service.setBit(bitKey, 85, true);
+			service.setBit(bitKey, 90, true);
+			System.out.println(bitKey + ", bit 73 : " + service.getBit(bitKey, 73));
+			System.out.println(bitKey + ", bit count : " + service.bitCount(bitKey));
+
 			// movies
 			this.service.setKey("moviescount", "0");
-	 		Map<String, String> actors = new HashMap<String, String>();
-	 		actors.put("actor1", "Jeff Bridges");
+			Map<String, String> actors = new HashMap<String, String>();
+			actors.put("actor1", "Jeff Bridges");
 			actors.put("actor2", "John Goodman");
 			actors.put("actor3", "John Turturro");
 			actors.put("actor4", "Steve Buscemi");
-	 		service.hset("movies:1998:The Big Lebowski", actors);
+			service.hset("movies:1998:The Big Lebowski", actors);
 			service.incr("moviescount");
-	 		actors.clear();
-	 		actors.put("actor1", "Billy Bob Thornton");
+			actors.clear();
+			actors.put("actor1", "Billy Bob Thornton");
 			actors.put("actor2", "Tony Cox");
 			actors.put("actor3", "Lauren Graham");
-	 		service.hset("movies:2003:Bad Santa", actors);
+			service.hset("movies:2003:Bad Santa", actors);
 			service.incr("moviescount");
-			
+
 			System.out.println("Total movies : " + service.getKey("moviescount"));
 		};
-  }
-	
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(RedisApplication.class, args);
 	}
--- 20-21/semester2/Webservices/Spring Boot Redis + REST/Spring-Redis/src/main/java/edu/ap/spring/controller/RedisController.java
+++ 20-21/semester2/Webservices/Spring Boot Redis + REST/Spring-Redis/src/main/java/edu/ap/spring/controller/RedisController.java
@@ -4,15 +4,85 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import edu.ap.spring.redis.RedisService;
 
 @Controller
 public class RedisController {
 
-    
+    private String CHANNEL = "edu:ap:redis";
+    private ArrayList<String> redisMessages = new ArrayList<String>();
+    @Autowired
+    private RedisService service;
 
- }
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/messages";
+    }
+
+    @GetMapping("/messages")
+    public String messages(Model model) {
+        model.addAttribute("messages", redisMessages);
+
+        return "messages";
+    }
+
+    // messaging
+    public void onMessage(String message) {
+        this.redisMessages.add(message);
+    }
+
+    @GetMapping("/message")
+    public String message() {
+        return "messageForm";
+    }
+
+    @PostMapping("/message")
+    public String saveMessage(@RequestParam("message") String message) {
+
+        service.sendMessage(CHANNEL, message );
+
+        return "redirect:/messages";
+    }
+
+    @GetMapping("/movies")
+    public String movies(Model model) {
+        
+        ArrayList<String> movieList = new ArrayList<String>();
+        Set<String> movies = service.keys("movies:*");
+        for(String m : movies) {
+            String movie = "";
+            // get hash with actors
+            Map<Object, Object> actors = service.hgetAll(m);
+            // get all parts of the key, eg : ["movies, "1998", "the Big Lebowski"]
+            String[] parts = m.split(":");
+
+            movie += parts[2] + " (" + parts[1] + ") ";
+            movie += "Actors: ";
+            // iterate over actors
+            StringBuilder b = new StringBuilder();
+            // k: key, v: value
+            actors.forEach((k, v) -> b.append(v + ", "));
+            movie += b.toString();
+            // strip off last
+            movie = movie.substring(0, movie.length() - 2);
+
+            movieList.add(movie);
+        }
+
+        model.addAttribute("moviescount", service.getKey("moviescount"));
+        model.addAttribute("movies", movieList);
+
+        return "movies";
+
+    }
+
+}
