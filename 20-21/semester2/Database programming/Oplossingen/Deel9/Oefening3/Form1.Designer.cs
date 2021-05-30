901b75ace0ad7cd64875bb5f0a9a2ffeab3cc0b8
Thomas Boyens <s115606@ap.be>
2021-05-30 12:44:48 +0200

    gitignore update

--- 20-21/semester2/Database programming/Oplossingen/Deel9/Oefening3/Form1.Designer.cs
+++ 20-21/semester2/Database programming/Oplossingen/Deel9/Oefening3/Form1.Designer.cs
@@ -36,25 +36,28 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(17, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.Size = new System.Drawing.Size(61, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Top 100";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(70, 13);
+            this.textBox1.Location = new System.Drawing.Point(93, 16);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(372, 389);
+            this.textBox1.Size = new System.Drawing.Size(495, 478);
             this.textBox1.TabIndex = 1;
             // 
             // buttonSearch
             // 
-            this.buttonSearch.Location = new System.Drawing.Point(367, 408);
+            this.buttonSearch.Location = new System.Drawing.Point(489, 502);
+            this.buttonSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonSearch.Name = "buttonSearch";
-            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.Size = new System.Drawing.Size(100, 28);
             this.buttonSearch.TabIndex = 2;
             this.buttonSearch.Text = "Search";
             this.buttonSearch.UseVisualStyleBackColor = true;
@@ -62,12 +65,13 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(454, 454);
+            this.ClientSize = new System.Drawing.Size(605, 559);
             this.Controls.Add(this.buttonSearch);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Oefening 3";
             this.ResumeLayout(false);
