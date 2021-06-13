8e57713c34f7e394ef3843503857ecf996402414
thomas boyens <thomas.boyens@hotmail.com>
2021-06-13 21:20:33 +0200

    TennisAPI

--- 20-21/semester2/WebProgramming/Week 10/TennisAPI/server.js
+++ 20-21/semester2/WebProgramming/Week 10/TennisAPI/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const bp = require('body-parser');
 const app = express()
-const trips = require("./routes/trips")
+const spelers = require("./routes/spelers")
 
 app.use(bp.urlencoded({ extended: false }));
 app.use(bp.json());
 
-app.use("/trips",trips)
+app.use("/spelers",spelers)
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/www/index.html")
+})
 
 app.listen(8080, () => console.log('Server ready'))
